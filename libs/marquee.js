e51d61e3663a41bd184f0b78f84cb8ff948b8f5d
shenyu616 <shenyu616@24money.com>
2019-06-10 17:34:54 +0800

    jquery.fn添加创建幻灯片方法

--- libs/marquee.js
+++ libs/marquee.js
@@ -1,5 +1,22 @@
-$.fn.marquee = function() {
-  // 可翻页
-  // popup when click
-  // 可以用 fancybox
+function getPagenation (pageNums) {
+  var $pagenation = '<div class="marquee-pagenation">';
+  for (var i = 0; i < pageNums; i++) {
+    $pagenation += `<i data-key="${i}"></i>`;
+  }
+  $pagenation += '</div>';
+  return $pagenation;
 }
+
+$.fn.marquee = function () {
+  var $marquee = $ (this);
+  $marquee.addClass ('marquee-container');
+  var pageNums = $marquee.find ('a').length;
+  let width = $ ($marquee.find ('a')[0]).outerWidth ();
+  $marquee.append (getPagenation (pageNums));
+
+  $marquee.find ('.marquee-pagenation i').bind ('click', function () {
+    $ ('.marquee-content').animate ({
+      left: `-${Number ($ (this).data ('key')) * width}px`,
+    });
+  });
+};
