2fc8aae2b03fb68928c529471f5cbf814091df88
shiyou <8533334@qq.com>
2019-06-11 21:23:24 +0800

    第一次作业by施佑

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-$('#marquee').marquee()
+$(function () {
+    $('#marquee').marquee({
+        imgList:[
+            "https://img.alicdn.com/tps/i4/TB1f8_hbMKG3KVjSZFLSuuMvXXa.jpg_q90_.webp",
+            "https://img.alicdn.com/tps/i4/TB18LGBbMaH3KVjSZFpSuvhKpXa.jpg_q90_.webp",
+            "https://img.alicdn.com/simba/img/TB1gCYda25G3KVjSZPxSuvI3XXa.jpg"
+        ],
+        width: '520',
+        height: '280',
+        interval: 5000,
+        auto: true
+    });
+});
