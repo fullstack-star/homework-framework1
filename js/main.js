902c0262af117b1ba7e0b64f7b0eaaf357ca0d44
李珣 <lixun@promote.cache-dns.local>
2019-06-12 02:28:00 +0800

    模仿fancybox的功能实现

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-$('#marquee').marquee()
+$('#marquee').marquee(['https://w1.hoopchina.com.cn/38/92/af/3892af23aff2dab16fd50a7dabc6a0cd001.jpg',
+'https://c2.hoopchina.com.cn/uploads/star/event/images/190611/bmiddle-3cdc655233300303139ee96feaa9b508537ff2aa.jpg?x-oss-process=image/resize,w_800/format,webp',
+'https://i1.hoopchina.com.cn/hupuapp/bbs/219/40243219/thread_40243219_20190611222754_s_110166_o_w_1024_h_686_24507.jpg?x-oss-process=image/resize,w_800/format,webp',
+'https://i2.hoopchina.com.cn/blogfile/201906/11/BbsImg_263688482793184_1560217023_s_277399_o_w_1890_h_956_85907.jpg?x-oss-process=image/resize,w_800/format,webp',
+'https://i2.hoopchina.com.cn/blogfile/201906/11/BbsImg_275865592536438_1560223854_s_186443_o_w_1658_h_1282_61784.jpg?x-oss-process=image/resize,w_800/format,webp',
+'https://c1.hoopchina.com.cn/uploads/star/event/images/190611/bmiddle-85099d27f8bfd76cbec339309975cac0cf4a5cf8.jpg?x-oss-process=image/resize,w_800/format,webp',
+'https://i11.hoopchina.com.cn/hupuapp/bbs/34/31738034/thread_31738034_20190511064708_s_32084_w_640_h_426_28186.jpeg?x-oss-process=image/resize,w_800/format,webp'])
--- libs/marquee.js
+++ libs/marquee.js
@@ -1,5 +1,68 @@
-$.fn.marquee = function() {
+$.fn.marquee = function(data) {
   // 可翻页
   // popup when click
   // 可以用 fancybox
+
+  // 向body添加预览图片及遮罩层
+  $('body').append(`<div id="mask">
+  <a class="previous" href="###">&lt;</a>
+  <img id="preview-image" src="">
+  <a class="next" href="###">&gt;</a>
+</div>`)
+  // 渲染图片列表
+  function render(images) {
+    let imgs = images.map(image => `
+      <li>
+        <img src="${image}"/>
+      </li>    
+    `.trim());
+    return `<ul id='list'>${imgs.join('')}</ul>`
+  }
+  $(this).html(render(data))
+
+  var $imgList = $("#list"),
+      $mask = $('#mask')
+      $previewImage = $mask.find('img'),
+      $previewPrevious = $mask.find('.previous'),
+      $previewNext = $mask.find('.next');
+
+  // 点击遮罩层隐藏
+  $mask.on('click', function(evt){
+    if(evt.target === this){
+      $mask.css("display" , 'none');
+    }
+  })
+
+  $imgList.on('click', function(evt){
+    if(evt.target.src) {
+      var src = evt.target.src,
+        idx = data.indexOf(src);
+      $mask.css("display" ,'block');
+      $previewImage.attr("src" ,src);
+      // 更新前后按钮状态
+      function updateButton(idx){
+        $previewPrevious.css("display" , idx ? 'block' : 'none');
+        $previewNext.css("display" , idx < data.length - 1 ? 'block' : 'none');
+      }
+      updateButton(idx);
+      // 前一个图片
+      $previewPrevious.on('click',function(){
+        $previewImage.attr("src" ,data[--idx]);
+        updateButton(idx);
+      })
+      // 下一张图片
+      $previewNext.on('click', function(){
+        $previewImage.attr("src" ,data[++idx]);
+        updateButton(idx);
+      })
+    }
+    
+  });
 }
+
+
+
+
+
+
+
