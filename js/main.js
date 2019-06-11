93e86524fda73a1b1279b569bc8cf9e26b82404f
hitler <1131367992@qq.com>
2019-06-11 11:46:51 +0800

    点击弹出放大图，翻页，翻页节流，动画效果，无缝轮播

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-$('#marquee').marquee()
+const imgs = [
+  { src: "./images/1.jpg", txt: "1111", url: "https://www.baidu.com" },
+  { src: "./images/2.jpg", txt: "2222", url: "https://www.baidu.com" },
+  { src: "./images/3.jpg", txt: "3333", url: "https://www.baidu.com" }
+];
+const opts = {
+  zoom: 1.5,
+  auto:true,
+  interval:1500,
+  animateEffect:{
+    speed:700,
+    easing:'swing'
+  }
+};
+$("#marquee").marquee(imgs, opts);
