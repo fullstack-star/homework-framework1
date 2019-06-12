442e179dfdcd3761759653228f472b8439c17f0b
cj348986512 <348986512@qq.com>
2019-06-12 11:29:20 +0800

    添加： 轮播效果

--- libs/marquee.js
+++ libs/marquee.js
@@ -1,5 +1,19 @@
 $.fn.marquee = function() {
   // 可翻页
   // popup when click
-  // 可以用 fancybox
+  var curIndex = 0;
+  var interval = setInterval( function() {
+    var $slides = $('.slide');
+    for ( var i = 0; i < $slides.length; i++) {
+      $('.slide').eq(i).addClass('hide');
+    }
+    curIndex++;
+    curIndex = curIndex > $slides.length - 1 ? 0 : curIndex;
+    $('.slide').eq(curIndex).removeClass('hide');
+  }, 2500);
+
+  $(this).on('click','img', function() {
+    // clearInterval(interval);
+    $(this).toggleClass('scale');
+  });
 }
