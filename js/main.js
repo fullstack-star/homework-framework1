966b5d59846c036d7c0d423fdbef61511dd4ae25
wsdfm <125838816@qq.com>
2019-06-12 14:03:21 +0800

    first commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-$('#marquee').marquee()
+var imgs = ['http://iph.href.lu/600x300?text=1', 'http://iph.href.lu/600x300?text=2', 'http://iph.href.lu/600x300?text=3']
+$('#marquee').marquee({
+  imgs: imgs,
+  arrow: 'always'
+})
