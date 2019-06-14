4498394812e71733645b882e920edb45fa5adf95
jinchengli <l@jinchenglideMacBook-Pro.local>
2019-06-14 19:18:41 +0800

    轮播图

--- js/main.js
+++ js/main.js
@@ -1,1 +1,70 @@
-$('#marquee').marquee()
+$(document).ready(function(){
+  function init() {
+    getHost();
+    /*$(".focus_item_lk").fancybox({
+     'transitionIn': 'none',
+     'transitionOut': 'none',
+     'titlePosition': 'over',
+     'titleFormat': function (title, currentArray, currentIndex, currentOpts) {
+     return '<span class="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+     }
+     });*/
+    // $(".focus_item_lk").fancybox({
+    //   prevEffect: 'elastic',
+    //   nextEffect: 'elastic',
+    //   hideOnOverlayClick:false,
+    //   helpers: {
+    //     title: {
+    //       type: 'inside'
+    //     },
+    //     thumbs: {
+    //       width: 50,
+    //       height: 50
+    //     }
+    //   }
+    // });
+  }
+  function getHost(){
+    // var deferred = $.Deferred();
+    $.ajax({
+      url:"./config/index.json",
+      dataType:'json',
+      type:"GET",
+      success:function (data) {
+        var host=data.host.static;
+        setPicUrl(host)
+        // deferred.resolve(host);
+        }
+    })
+    // return deferred.promise()
+  }
+  function setPicUrl(host) {
+    $.ajax({
+      url:"./mock/getPicUrl.json",
+      dataType:'json',
+      type:"GET",
+      success:function (data) {
+        if(data.data.returnCode%1000===0){
+          var list=data.data&&data.data.list||[];
+          // var data=
+          var imgSrc=[];
+          list.forEach(function (item,index) {
+            if(index<=2){
+              imgSrc.push(host+item['url']);
+            }
+          })
+          $('#marquee').marquee({
+            imgSrc:imgSrc
+          })
+        }
+      }
+    })
+  }
+  $("body").delegate(".focus_item_img","click",function(e){
+    e.preventDefault();
+  });
+  init();
+});
+
+
+
--- libs/marquee.js
+++ libs/marquee.js
@@ -1,5 +1,98 @@
-$.fn.marquee = function() {
+$.fn.marquee = function (data) {
   // 可翻页
   // popup when click
   // 可以用 fancybox
+  var $box = $(this);
+  var timer;
+  var app = {
+    data:{
+      length:0,
+      currentIndex:0,
+      width:$box.find(".slider_item").eq(0).width(),
+      isEnlarged:false
+      // width:$box.width(),
+    },
+    el:{
+      $ul:$box.find(".slider_wrapper"),
+      $imgs_lk:$box.find(".focus_item_lk"),
+    },
+    init:function () {
+      app.setData();
+      app.bind_event();
+      app.showtime();
+      app.fancybox();
+    },
+    fancybox:function () {
+      app.el.$imgs_lk.fancybox(
+        {
+          beforeShow: function () {
+            app.data.isEnlarged=true
+          },
+          afterClose: function () {
+            app.data.isEnlarged=false;
+            app.showtime();
+          },
+        }
+      );
+    },
+    setData:function () {
+      console.log("data",data)
+      $box.find(".focus_item_lk").each(function (index,item) {
+        item.href=data['imgSrc'][index];
+        $(item).find("img")[0].src=data['imgSrc'][index];
+      })
+      app.data.length=$box.find(".focus_item_img").length;
+      $box.find(".slider_wrapper").width(app.data.length*app.data.width+10);
+    },
+    bind_event:function () {
+      $box
+        .delegate('.slider_control', 'click', function (e) {
+          e.preventDefault();
+          var _this = e.currentTarget;
+          if ($(_this).hasClass("slider_control_next")) {
+            app.data.currentIndex+=1;
+          } else if ($(_this).hasClass("slider_control_prev")) {
+            app.data.currentIndex-=1;
+          }else{
+            return;
+          }
+          app.jump();
+        })
+  /*      .delegate('.focus_item_lk','click',function (e) {
+          console.log("点击focus_item_lk")
+          e.preventDefault();
+          clearTimeout(timer);
+          app.data.isEnlarged=true;
+        })*/
+        .delegate('.slider_item',{
+          "mouseenter": function(e){
+            e.preventDefault();
+            console.log("移入")
+            clearTimeout(timer)
+          },
+          "mouseleave": function(e){
+            e.preventDefault();
+            console.log("移出")
+            if(!app.data.isEnlarged)app.showtime();
+          }
+        })
+    },
+    showtime: function () {
+      timer = setTimeout(function () {
+        app.data.currentIndex+=1;
+        app.jump();
+        app.showtime();
+      }, 2000) //每1.5秒
+    },
+    getIndex:function () {
+      if (app.data.currentIndex==-1) {app.data.currentIndex=app.data.length-1;}
+      if (app.data.currentIndex==app.data.length) {app.data.currentIndex=0;}
+    },
+    jump:function () {
+      app.getIndex();
+      console.log("jump",app.data.currentIndex)
+      app.el.$ul.animate({left:-app.data.currentIndex*app.data.width},400);
+    },
+  }
+  app.init();
 }
